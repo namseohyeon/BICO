ab73b668cc6e7d00be05e09b9c9178cb001c7314
namseohyeon <syn5421a@gamil.com>
2022-06-19 11:33:23 +0900

    지도 api에 자전거도로 추가

--- app/src/main/java/com/example/bico/LocationActivity.kt
+++ app/src/main/java/com/example/bico/LocationActivity.kt
@@ -8,7 +8,6 @@ import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 import java.util.*
-import com.example.bico.realAdapter
 
 
 class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -27,18 +26,47 @@ class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView = findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
-
         locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
 
     }
 
 
     override fun onMapReady(@NonNull naverMap: NaverMap) {
+//        this.naverMap = naverMap
+//        naverMap.locationSource = locationSource
+//        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+//        naverMap.setMapType(NaverMap.MapType.Basic);
+//
+//        naverMap.maxZoom=18.0
+//        naverMap.minZoom=10.0
+//        this.naverMap = naverMap
+//        naverMap.locationSource = locationSource
+//        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+//        val cameraPosition = CameraPosition(
+//            LatLng(33.38, 126.55),  // 위치 지정
+//            15.0 // 줌 레벨
+//        )
+//        naverMap.cameraPosition = cameraPosition
         this.naverMap = naverMap
+
+        //배경 지도 선택
+        naverMap.mapType = NaverMap.MapType.Basic
+
         naverMap.locationSource = locationSource
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
-        naverMap.maxZoom=18.0
-        naverMap.minZoom=10.0
+
+        //건물 표시
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BICYCLE, true)
+
+        //위치 및 각도 조정
+
+        //위치 및 각도 조정
+        val cameraPosition = CameraPosition(
+            LatLng(naverMap.cameraPosition.target.latitude,
+                naverMap.cameraPosition.target.longitude),  // 위치 지정
+            15.0,  // 줌 레벨
+        )
+        naverMap.cameraPosition = cameraPosition
 
     }
 
@@ -77,32 +105,32 @@ class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onLowMemory()
         mapView.onLowMemory()
     }
-    fun getCurrentPosition(naverMap: NaverMap): LatLng {
-        val cameraPosition = naverMap.cameraPosition
-        return LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude)
-    }
-
-    val REFERANCE_LAT = 1 / 109.958489129649955
-    val REFERANCE_LNG = 1 / 88.74
-    val REFERANCE_LAT_X3 = 3 / 109.958489129649955
-    val REFERANCE_LNG_X3 = 3 / 88.74
-    fun withinSightMarker(currentPosition: LatLng, markerPosition: LatLng): Boolean {
-        val withinSightMarkerLat =
-            Math.abs(currentPosition.latitude - markerPosition.latitude) <= REFERANCE_LAT_X3
-        val withinSightMarkerLng =
-            Math.abs(currentPosition.longitude - markerPosition.longitude) <= REFERANCE_LNG_X3
-        return withinSightMarkerLat && withinSightMarkerLng
-    }
-
-    // 지도상에 표시되고있는 마커들 지도에서 삭제
-    private fun freeActiveMarkers() {
-        if (activeMarkers == null) {
-            activeMarkers = Vector()
-            return
-        }
-        for (activeMarker in activeMarkers!!) {
-            activeMarker.map = null
-        }
-        activeMarkers = Vector()
-    }
+//    fun getCurrentPosition(naverMap: NaverMap): LatLng {
+//        val cameraPosition = naverMap.cameraPosition
+//        return LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude)
+//    }
+//
+//    val REFERANCE_LAT = 1 / 109.958489129649955
+//    val REFERANCE_LNG = 1 / 88.74
+//    val REFERANCE_LAT_X3 = 3 / 109.958489129649955
+//    val REFERANCE_LNG_X3 = 3 / 88.74
+//    fun withinSightMarker(currentPosition: LatLng, markerPosition: LatLng): Boolean {
+//        val withinSightMarkerLat =
+//            Math.abs(currentPosition.latitude - markerPosition.latitude) <= REFERANCE_LAT_X3
+//        val withinSightMarkerLng =
+//            Math.abs(currentPosition.longitude - markerPosition.longitude) <= REFERANCE_LNG_X3
+//        return withinSightMarkerLat && withinSightMarkerLng
+//    }
+//
+//    // 지도상에 표시되고있는 마커들 지도에서 삭제
+//    private fun freeActiveMarkers() {
+//        if (activeMarkers == null) {
+//            activeMarkers = Vector()
+//            return
+//        }
+//        for (activeMarker in activeMarkers!!) {
+//            activeMarker.map = null
+//        }
+//        activeMarkers = Vector()
+//    }
 }
